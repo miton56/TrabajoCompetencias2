4de87c41378d1ec7290bd9ec6b12a4e673203150
simon barros <simonbarros56@gmail.com>
2025-08-28 18:27:45 -0400

    inicio proyecto

---
+++ poryecto_competencias2/src/main/java/com/mycompany/poryecto_competencias2/Poryecto_competencias2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poryecto_competencias2;
+
+/**
+ *
+ * @author Willow
+ */
+public class Poryecto_competencias2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
